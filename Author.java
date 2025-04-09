6724ea163aad9907c8b690287265658b08aa9a4c
TEAMPROJECT21221 <omarnedal84@gmail.com>
2025-04-09 07:24:34 +0100

    Create Author.java

---
+++ Author.java
@@ -0,0 +1,27 @@
+// Author.java
+package com.example.library.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Author {
+    @Id
+    private Long id;
+    private String name;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
