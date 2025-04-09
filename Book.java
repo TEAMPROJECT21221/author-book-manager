012a165a360e8dc5811a6fd314d50bc1da88b5e3
TEAMPROJECT21221 <omarnedal84@gmail.com>
2025-04-09 07:24:56 +0100

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,36 @@
+// Book.java
+package com.example.library.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Book {
+    @Id
+    private Long id;
+    private String title;
+    private Long authorId;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+}
